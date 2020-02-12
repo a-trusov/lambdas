c0ded319a4750b3d5160d145665926cf47449635
Andrey Trusov <trusov@haulmont.com>
2020-02-12 11:37:22 +0400

    parse people json

---
+++ src/main/java/example/services/DateService.java
@@ -0,0 +1,26 @@
+package example.services;
+
+import com.google.common.io.Resources;
+import com.google.gson.Gson;
+
+import com.google.gson.reflect.TypeToken;
+import example.model.Person;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class DateService {
+
+    public static List<Person> getPeople() throws IOException {
+        InputStream inputStream = Resources.getResource("people.json").openStream();
+        String json = IOUtils.toString(inputStream);
+
+        List<Person> people = new Gson().fromJson(json,
+                new TypeToken<List<Person>>() {}.getType());
+        return people;
+    }
+
+
+}
--- src/test/java/org/example/LambdasTest.java
+++ src/test/java/org/example/LambdasTest.java
@@ -1,11 +1,20 @@
 package org.example;
 
+import example.model.Person;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static example.services.DateService.getPeople;
+
 
 public class LambdasTest {
 
     @Test
-    public void test() {
-
+    public void test() throws IOException {
+        List<Person> peoples = getPeople();
+        Assertions.assertThat(peoples).hasSize(1000);
     }
 }
